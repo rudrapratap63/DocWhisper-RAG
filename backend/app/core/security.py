17a0624ee5cb638cce20c39fb9ab666a35887bcb
Rudra Pratap Singh <rudra888156@gmail.com>
2026-04-02 12:17:02 +0530

    add security module for password hashing and JWT token creation

---
+++ backend/app/core/security.py
@@ -0,0 +1,19 @@
+from pwdlib import PasswordHash
+import jwt
+from datetime import datetime, timedelta, timezone
+from app.core.config import settings
+
+password_hash = PasswordHash.recommended()
+
+def hash_password(password: str) -> str:
+    return password_hash.hash(password)
+
+def verify_password(plain_password: str, hashed_password) -> bool:
+    return password_hash.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(float(settings.ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
